3450cb17b2848e5949445526d458206d7cc63668
augustine <augustino0890@gmail.com>
2022-04-20 14:08:59 +0900

    go: type assertion

--- learn-go/go-scratch/data_types.go
+++ learn-go/go-scratch/data_types.go
@@ -1,6 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Stringer interface {
+	String() string
+}
+
+type fakeString struct {
+	content string
+}
+
+// function used to implement the Stringer interface
+func (s *fakeString) String() string {
+	return s.content
+}
+
+func printString(value interface{}) {
+	switch str := value.(type) {
+	case string:
+		fmt.Println(str)
+	case Stringer:
+		fmt.Println(str.String())
+	}
+}
+
+func timeMap(y interface{}) {
+	z, ok := y.(map[string]interface{})
+	if ok {
+		z["updated_at"] = time.Now()
+	}
+}
 
 func main() {
 	var n1 uint8
@@ -18,4 +50,14 @@ func main() {
 	var n4 uint64
 	n4 = 279283782735793
 	fmt.Println(n4)
+
+	foo := map[string]interface{}{
+		"Matt": 42,
+	}
+	timeMap(foo)
+	fmt.Println(foo)
+
+	printString("Hello, Gophers")
+	s := &fakeString{"Ceci n'est pas um string"}
+	printString(s)
 }
