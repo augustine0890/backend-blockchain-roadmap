21527c33358e7abcb4dc430b97eb3fb07faca528
augustine <augustino0890@gmail.com>
2022-07-20 18:01:15 +0900

    go (generic): updated tree

--- learn-go/go-scratch/generics/tree/tree.go
+++ learn-go/go-scratch/generics/tree/tree.go
@@ -1,31 +1,22 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
-type Orderable interface {
-	Order(interface{}) int
+type Orderable[T any] interface {
+	Order(T) int
 }
 
-type OrderableInt int
-
-func (oi OrderableInt) Order(val interface{}) int {
-	return int(oi - val.(OrderableInt))
+type Tree[T Orderable[T]] struct {
+	val         T
+	left, right *Tree[T]
 }
 
-type OrderableString string
-
-func (os OrderableString) Order(val interface{}) int {
-	return strings.Compare(string(os), val.(string))
-}
-
-type Tree struct {
-	val         Orderable
-	left, right *Tree
-}
-
-func (t *Tree) Insert(val Orderable) *Tree {
+func (t *Tree[T]) Insert(val T) *Tree[T] {
 	if t == nil {
-		return &Tree{val: val}
+		return &Tree[T]{val: val}
 	}
 
 	switch comp := val.Order(t.val); {
@@ -37,9 +28,40 @@ func (t *Tree) Insert(val Orderable) *Tree {
 	return t
 }
 
+func (t *Tree[T]) Contains(val T) bool {
+	if t == nil {
+		return false
+	}
+	switch comp := val.Order(t.val); {
+	case comp < 0:
+		return t.left.Contains(val)
+	case comp > 0:
+		return t.right.Contains(val)
+	default:
+		return true
+	}
+}
+
+type OrderableInt int
+
+func (oi OrderableInt) Order(val OrderableInt) int {
+	return int(oi - val)
+}
+
+type OrderableString string
+
+func (os OrderableString) Order(val OrderableString) int {
+	return strings.Compare(string(os), string(val))
+}
+
 func main() {
-	var it *Tree
+	var it *Tree[OrderableInt]
 	it = it.Insert(OrderableInt(5))
 	it = it.Insert(OrderableInt(3))
-	it = it.Insert(OrderableString("not compile"))
+	fmt.Println(it.Contains(2))
+	fmt.Println(it.Contains(3))
+	a := 10
+	it = it.Insert(OrderableInt(a))
+	// uncomment to see a compile-time error
+	// it = it.Insert(OrderableString("not compile"))
 }
