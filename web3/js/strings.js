67130b8b8be6eeac70a57832eace62e52dd07dc6
augustine <augustino0890@gmail.com>
2022-08-08 15:03:23 +0900

    js: strings

---
+++ web3/js/strings.js
@@ -0,0 +1,3 @@
+var sentence = "Javascript strings are useful for storing text.\n";
+var tabbedSentence = "\tJavascript strings are also useful for manipulating text."
+console.log(sentence + tabbedSentence);
