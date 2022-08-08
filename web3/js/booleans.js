17cbdb977ead15aa66862676f03343a89ace3cfd
augustine <augustino0890@gmail.com>
2022-08-08 15:39:08 +0900

    js: booleans and undefined/null

---
+++ web3/js/booleans.js
@@ -0,0 +1,14 @@
+var theTruth = true;
+var aLie = false;
+
+var ageOfBill = 11;
+var ageOfSally = "11";
+console.log(ageOfBill == ageOfSally);
+console.log(ageOfBill === ageOfSally);
+console.log(ageOfBill !== ageOfSally);
+
+var declareValue;
+console.log(declareValue); // undefined value
+
+var nothing = null;
+console.log(nothing);
