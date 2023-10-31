96a13ef83468332114b5d49e97d4bd1ca445803b
augustine <augustino0890@gmail.com>
2023-10-31 18:03:23 +0900

    stack

--- learn-js/algorithms/stack.js
+++ learn-js/algorithms/stack.js
@@ -1,8 +1,40 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 class Stack {
-    constructor() {
-        this.maxSize = 100;
-        this.stack = [];
+    constructor(value) {
+        const newNode = new Node(value);
+        this.top = newNode;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (this.length === 0) return undefined;
+        let temp = this.top;
+        this.top = this.top.next;
+        temp.next = null;
+        this.length--;
+        return temp;
     }
 }
 
-export default Stack;
+let myStack = new Stack(11);
+myStack.push(23);
+myStack.push(2);
+myStack.push(12);
+console.log(myStack);
