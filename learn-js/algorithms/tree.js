1947b22c840e81a9a6618845f371f4154aeb7106
augustine <augustino0890@gmail.com>
2023-11-01 16:34:54 +0900

    insert tree

---
+++ learn-js/algorithms/tree.js
@@ -0,0 +1,44 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else if (value > currentNode.value) {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+        }
+    }
+}
+
+let myTree = new BinarySearchTree();
+myTree.insert(10);
+console.log(myTree);
